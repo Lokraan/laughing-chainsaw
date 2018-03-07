f1c495bdb84179c931d817495b8f4a1f61b4c8d6
Lokraan <Artinaire@gmail.com>
2018-03-06 22:21:13 -0600

    rename for clarity

--- helpers/message_processor.py
+++ helpers/message_processor.py
@@ -4,7 +4,17 @@ import ccxt
 import re
 
 
-class CommandProcessor:
+class MessageProcessor:
+	"""
+	Class to process messages and repsond to basic ones that don't require
+	processing.
+
+	Attributes:
+		_client: client used to communicate with discord
+		_logger: logger used to log events
+		_bot: bot used to process commands
+		_db: db used to get server preferences for commands 
+	"""
 	def __init__(self, client, bot, logger, db):
 		self._client = client
 		self._logger = logger
@@ -13,6 +23,17 @@ class CommandProcessor:
 
 
 	def is_admin(self, message: discord.Message) -> bool:
+		"""
+		Checks if the person who sent the message is an admin.
+
+		Args:
+			message: message sent.
+
+		Returns:
+			true if admin, false if not
+
+		"""
+
 		chann = message.channel
 		permissions = chann.permissions_for(user)
 
@@ -20,6 +41,25 @@ class CommandProcessor:
 
 
 	async def process_message(self, message: discord.Message) -> None:
+		"""
+		Processes message based on the message's server's preferences.
+
+		Commands it processed are
+		| Command  | Description|
+		| :------: | ---------- |
+		| `$start <exchanges>`  | Starts checking the exchanges for price/rsi updates in the channel the message was sent. *Uses bittrex by default*| 
+		| `$stop <exchanges>`   | Stops checking the exchanges for price/rsi updates in the channel the message was sent.  *Uses bittrex by default*| 
+		| `$prefix <prefix>`    | Sets the prefix for the current server to the prefix specified. *Only works for users with admin privileges*      |
+		| `$price`  | Gets market data for currency specified after, ie `$price eth` |
+		| `$cap`    | Gets the marketcap of cryptocurrencies as a whole.             |
+		| `$help`   | Private messages user bot commands and github .                |
+		| `$greet`  | Greets whoever wants to be greeted. |
+		| `$source` | Prints the link to this repository. |	
+
+		Args:
+			message: message sent and to be processed
+
+		"""
 		# Default greet
 
 		content = message.content
@@ -89,7 +129,7 @@ class CommandProcessor:
 
 			elif cmd == "prefix":
 				if self.is_admin(message) and len(params) > 0:
-					await self._bot.change_prefix(message, params)
+					await self._bot.change_prefix(message, params[0])
 
 			elif cmd in ccxt.exchanges:
 				await self._bot.price(message, markets, exchange=getattr(ccxt, exchange))
